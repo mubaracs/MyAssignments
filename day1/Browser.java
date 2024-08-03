a0a059a33cd926efe2729d20f8c1e5709e1fb5ed
mubaracs <mubaracs@gmail.com>
2024-08-02 23:05:03 -0500

    Add files via upload
    day 1

---
+++ day1/Browser.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Browser {
+
+	int age = 30;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int noOfTabsOpen = 3;
+	boolean checkBrowserLoaded=true;
+	char checkFirstLetteroftheBrowser='C';
+	String browserName="Chrome";
+	float browserVersion=123.45f;
+	long phone=9876543210l;
+	
+	System.out.println("Number of open tabs presented :" + noOfTabsOpen);
+	System.out.println("Browser loaded:"+checkBrowserLoaded);
+	//System.out.println("Browser name:" +browserName +"n\" );
+	
+			
+	
+	}
+
+}
---
+++ day1/Firefox.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float browserVersion = 100.2f;
+		String browserName = "firefox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo = 'f';
+		
+		System.out.println("Browser Version:" +browserVersion);
+		System.out.println("Browser Name:" +browserName);
+		System.out.println("Browser visible:" +isVisible);
+		System.out.println("Browser release year:" +releaseYear);
+		System.out.println("Browser logo:" +browserLogo);
+		
+		
+		
+		
+	}
+
+}
